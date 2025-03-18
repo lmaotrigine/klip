e02f780a93a5fbf339521d7be0353928d1e6c8dc
Isis Ebsen <root@5ht2.me>
2025-03-18 13:45:27 +0000

    brew: update tap

--- pkg/brew/klip-bin.rb
+++ pkg/brew/klip-bin.rb
@@ -1,19 +1,22 @@
+# typed: strict
+# frozen_string_literal: true
+
+# Homebrew formula for klip using binaries from GitHub releases
 class KlipBin < Formula
-  version '0.1.0'
-  desc 'Copy/paste anything over the network'
-  homepage 'https://github.com/lmaotrigine/klip'
+  desc "Copy/paste anything over the network"
+  homepage "https://github.com/lmaotrigine/klip"
+  version "0.1.0"
   if OS.mac?
-    url = "https://github.com/lmaotrigine/klip/releases/download/v#{version}/klip-x86_64-apple-darwin.tar.xz"
-    sha256 "somesha"
+    url "https://github.com/lmaotrigine/klip/releases/download/#{version}/klip-x86_64-apple-darwin.full.zip"
+    sha256 "80c4824aad513a2735eb4cb91b42950bf01fa613dcb2ee24b7012660a0523669"
   elsif OS.linux?
-    url = "https://github.com/lmaotrigine/klip/releases/download/v#{version}/klip-x86_64-unknown-linux-musl.tar.xz"
-    sha256 "somesha"
+    url "https://github.com/lmaotrigine/klip/releases/download/#{version}/klip-x86_64-unknown-linux-musl.full.tar.xz"
+    sha256 "c976f3f33745057318bb6260614e43c70a6f8b8698b069fa0eaa863cc2dbb40e"
   end
-  conflicts_with "klip"
   def install
     bin.install "klip"
     man1.install "doc/klip.1"
-    bash_completion.install "complete/klip.bash"
-    zsh_completion.install "complete/_klip"
+    bash_completion.install "completions/klip.bash"
+    zsh_completion.install "completions/_klip"
   end
 end
