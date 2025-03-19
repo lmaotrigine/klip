c347e25921172f05879495860d7750c26c5763f0
Isis Ebsen <root@5ht2.me>
2025-03-19 08:43:10 +0000

    drop dependency on flake-utils

--- flake.nix
+++ flake.nix
@@ -2,15 +2,31 @@
   description = "klip";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     crane.url = "github:ipetkov/crane";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
-    flake-utils.lib.eachDefaultSystem
+  outputs = { self, nixpkgs, rust-overlay, crane }:
+    let
+      eachSystem = systems: f:
+        let
+          op = attrs: system:
+            let
+              ret = f system;
+              op = attrs: key: attrs // {
+                ${key} = (attrs.${key} or { }) // {
+                  ${system} = ret.${key};
+                };
+              };
+            in
+            builtins.foldl' op attrs (builtins.attrNames ret);
+        in
+        builtins.foldl' op { } systems;
+      eachDefaultSystem = eachSystem [ "aarch64-linux" "aarch64-darwin" "x86_64-linux" "x86_64-darwin" ];
+    in
+    eachDefaultSystem
       (system:
         let
           overlays = [ (import rust-overlay) ];
